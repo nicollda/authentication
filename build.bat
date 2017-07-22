44fe746d22bacb79b48fc5440505dbd71cc83135
nicollda <dnicoll@ca.ibm.com>
2017-07-22 02:25:42 -0300

    commit

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ go build ./myHL/
 IF NOT ERRORLEVEL 1 (
 	go clean ./myHL/
 	git commit -m "commit" -a
-	git push https://github.com/nicollda/authentication
+	git push --set-upstream https://github.com/nicollda/authentication master
 	c:\curl\curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://7993495d15b440378dff6a89aaa0b616-vp2.us.blockchain.ibm.com:5002/chaincode" > serviceKey.txt
 )
 
