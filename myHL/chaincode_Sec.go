369068610bff3bdb34190f71bf146907e8a15ca3
nicollda <dnicoll@ca.ibm.com>
2017-07-24 14:09:59 -0300

    commit

--- myHL/chaincode_Sec.go
+++ myHL/chaincode_Sec.go
@@ -125,15 +125,24 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	
 	t.bl.initObjects(stub) //for some reason the stub changes each call
 	
-	if function == "authenticate" {
+	if function == "Button 1" {
+		fmt.Printf("Function is authenticate")
+		
+		if len(args) != 1 {
+			return nil, errors.New("Incorrect number of arguments. Expecting 2")
+		}
+		
+		return []byte("This is the role 1 text"), nil
+	
+	} else if function == "authenticate" {
 		fmt.Printf("Function is authenticate")
 		
 		if len(args) != 2 {
 			return nil, errors.New("Incorrect number of arguments. Expecting 2")
 		}
-		//return []byte("[\"Button1\",\"Button2\"]"), nil
-		return t.bl.authenticate(args[0], args[1])
 		
+		return t.bl.authenticate(args[0], args[1])
+				
 	} else if function == "init" {
 		fmt.Printf("Function is init")
 		return t.Init(stub, function, args)
